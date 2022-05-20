0a3d390d66a2caffe2bc0b4bc71fa24817153cf0
ngocminh1605 <100121817+ngocminh1605@users.noreply.github.com>
2022-05-20 15:41:28 +0700

    Add files via upload

---
+++ Bansung/BaseFunction.h
@@ -0,0 +1,72 @@
+﻿#ifndef BASE_FUNCTION_H_
+#define BASE_FUNCTION_H_
+
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+
+// định nghĩa các biến
+// stactic là biến dạng global
+static SDL_Window* g_window = NULL; // là struct giữ tất cả các thông tin của cửa sổ mà chúng ta tạo ra như size, postion, border, fullscreen.
+static SDL_Event g_sukien; // union chứa tất cả các thông tin của tất cả các event trong bộ thư viện ví dụ như từ bàn phím, chuột,..
+static SDL_Renderer* g_manhinh = NULL; //  là struct xử lý tất cả các công việc rendering lên cửa sổ SDL_Window.
+
+//Thông số màn Hình
+
+const int FPS = 25; // fps là frame per second, số khung hình frame trong một khoảng thời gian
+const int DOCAO_MANHINH = 640; // Màn hình có độ cao cố định 640 pixel
+const int DORONG_MANHINH = 1280; // Màn hình có độ rộng mặc định 1280 pixel
+const int SCREEN_BPP = 32; // Bit per Pixel, 32 BPP = 4294967296 màu
+
+// Khai báo màu sắc để transparent ( làm trong suốt ) nền các hình ảnh
+
+const int Color_R = 167;
+const int Color_G = 175;
+const int Color_B = 180;
+
+
+// bản đồ
+#define KICHTHUOC_O_LUOI 64 // cả bản đồ được chia thành một dạng lưới gồm nhiều ô vuông với mỗi ô có kích thước 64x64
+
+#define COT_X_MAX 400 // Số cột theo chiều dọc X tối đa là 400
+#define HANG_Y_MAX 10 // Số hàng theo chiều ngang Y là 10. Bằng với chiều cao ban đầu khai báo 64x10 = 640 = DOCAO_MANHINH Với 400 cột và 10 hàng thì tổng ra có 4000 ô vuông kích thước 64 trên một bản dồ dạng lưới, mỗi một ô sẽ fill giá trị tương ứng bằng hình ảnh như mặt đất, tường, nhân vật hoặc đường đạn
+
+
+// Tạo cấu trúc dữ liệu trạng thái, vị trí và chỉ số cho từng ô trong lưới
+// dành cho phần bản đồ
+typedef struct BanDo
+{
+	// vị trí của ô vuông
+	int vitri_x; // start_x_
+	int vitri_y; // start_y_
+
+	// chỉ số 
+	int chiso_x; // max_x_
+	int chiso_y; // max_y_
+
+	// mảng 2 chiều lưu toàn bộ chỉ cố và vị trí của tất cả các ô
+	int viengach[HANG_Y_MAX][COT_X_MAX];
+
+	// file lưu thông số
+
+	char* file_;
+};
+
+
+// dành cho phần nhân vật
+// khai báo một kiểu dữ liệu nhận các giá trị đầu vào từ bàn phím như lên xuống trái phải mục tiêu sử dụng để di chuyển nhân vật
+
+typedef struct DauVaoDiChuyen
+{
+	int trai_;
+	int phai_;
+	int len_;
+	int xuong_;
+	int nhay_;
+};
+
+
+#endif // !BASE_FUNCTION_H_
+
---
+++ Bansung/BaseObject.cpp
@@ -0,0 +1,67 @@
+﻿#include "stdafx.h"
+#include "BaseObject.h"
+
+BaseObject::BaseObject()
+{
+	p_doituong_ = NULL;
+	rect_.w = 0;
+	rect_.h = 0;
+	rect_.x = 0;
+	rect_.y = 0;
+}
+
+BaseObject::~BaseObject()
+{
+	GiaiPhong(); // khi kết thúc chương trình sẽ hủy các đối tượng tránh việc còn dữ liệu khiến chiếm dụng bố nhớ
+}
+
+bool BaseObject::TaiAnh(std::string duongdan, SDL_Renderer* manhinh)
+{
+	GiaiPhong();  // cai cho nay bai 8, hay bai 9 co noi den, nhung anh them truo vao cho em
+	// final varibal texture
+	SDL_Texture* newTexture = NULL; // mặc định là đúng
+
+	// tải ảnh lên từ đường dẫn chỉ định
+	SDL_Surface* loadedSurface = IMG_Load(duongdan.c_str());// hàm IMG_Load tải ảnh lên trả về surface sau đó đưa surface thành texture
+	if (loadedSurface != NULL) // kiểm tra
+	{
+		// transparent background cho các tấm ảnh
+		SDL_SetColorKey(loadedSurface, 
+						SDL_TRUE, 
+						SDL_MapRGB(loadedSurface->format, Color_R, Color_G, Color_B));
+
+		// khởi tạo texture từ surface pixel
+		newTexture = SDL_CreateTextureFromSurface(manhinh, loadedSurface);
+		if (newTexture != NULL) // bây h, tại thời điểm cần khởi tạo, có nghĩ newTexture phải khác rỗng ms là đúng
+		{
+			// nhận dạng kích thước của ảnh mà đã tải lên
+			rect_.w = loadedSurface->w;
+			rect_.h = loadedSurface->h;
+		}
+		// Khi này toàn bộ thông số và thuộc tính của hình ảnh tải lên đang nằm trong biến newTexture
+
+		// loại loadedSurface cũ vì thông số của ảnh đã được newTexture lưu 
+		SDL_FreeSurface(loadedSurface);
+	}
+
+	p_doituong_ = newTexture;
+	return p_doituong_ != NULL; // nếu không có giá trị trả về 0 là false còn có giá trị trả về là 1 tức là true
+}
+
+void BaseObject::Xuat(SDL_Renderer* x_manhinh, const	SDL_Rect* clip) // load ảnh lên đối tượng 
+{
+	SDL_Rect xuat_g = {rect_.x, rect_.y, rect_.w, rect_.h}; // x,y là vị trí của tấm ảnh. w,h là kích thước tấm ảnh
+
+	SDL_RenderCopy(x_manhinh, p_doituong_, clip, &xuat_g); // đẩy toàn bộ thông số của p_doituong_ lên x_manhinh vơi thông số và kích thước từ xuat_g
+}
+
+void BaseObject::GiaiPhong()
+{
+	if (p_doituong_ != NULL) 
+	{
+		SDL_DestroyTexture(p_doituong_);
+		p_doituong_ = NULL;
+		rect_.w = 0;
+		rect_.h = 0;
+	}
+}
