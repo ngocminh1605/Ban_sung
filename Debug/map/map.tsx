f123455e6b2c6a05251450977ea19b7b221995f8
ngocminh1605 <100121817+ngocminh1605@users.noreply.github.com>
2022-05-22 23:34:25 +0700

    Delete Debug/map directory

--- Debug/map/map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="279400871_1067188033879320_8242885928336339618_n" tilewidth="64" tileheight="64" tilecount="5" columns="5">
- <image source="../../../../../../../../Downloads/279400871_1067188033879320_8242885928336339618_n.png" width="320" height="64"/>
-</tileset>
